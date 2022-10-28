33399ea01af1ab68b84869a577defb5bfbbce74b
missestrella2 <missestrella2@gmail.com>
2022-10-28 11:37:47 -0300

    configuracion de ruteo

--- FRONTEND/src/app/app.module.ts
+++ FRONTEND/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
@@ -14,6 +15,7 @@ import { HabilidadesComponent } from './components/habilidades/habilidades.compo
 import { MisProyectosComponent } from './components/mis-proyectos/mis-proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -32,6 +34,7 @@ import { HttpClientModule} from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule
     HttpClientModule
   ],
   providers: [],
