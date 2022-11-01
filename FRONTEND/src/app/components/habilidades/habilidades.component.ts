5da0633cec86bf2ee64cb05f4f85572033d02821
missestrella2 <missestrella2@gmail.com>
2022-10-31 23:08:06 -0300

    -

--- FRONTEND/src/app/components/habilidades/habilidades.component.ts
+++ FRONTEND/src/app/components/habilidades/habilidades.component.ts
@@ -12,4 +12,26 @@ export class HabilidadesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // let valor1 = prompt("Dame el valor 1",0);
+
+
+//   function guardar(){
+
+//     var _idEmp = document.getElementById("idemp").value;
+
+
+//     var fila="<tr><td>"+_idEmp+"</td><td>"+_nombre+"</td><td>"+_apellido+"</td><td>"+_numemp+"</td><td>"+_area+"</td><td>"+_tipoUs+"</td></tr>";
+
+//     var btn = document.createElement("TR");
+//     btn.innerHTML=fila;
+//     document.getElementById("Usuarios").appendChild(btn);
+
+
+//      if (confirm('¿Agregar usuario nuevo?')) {
+//        document.form1.submit();
+//        document.getElementById("myForm").reset();
+//     }
+// }
+
+
 }
